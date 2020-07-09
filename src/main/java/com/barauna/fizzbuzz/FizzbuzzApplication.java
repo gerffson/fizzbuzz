910f9733d01ccc086303d07f9fbd85cc431a4ff8
Luiz Sergio <luizsergio.barauna@gmail.com>
2020-07-08 22:53:39 -0300

    Versao inicial do projeto

---
+++ src/main/java/com/barauna/fizzbuzz/FizzbuzzApplication.java
@@ -0,0 +1,13 @@
+package com.barauna.fizzbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbuzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barauna/fizzbuzz/FizzbuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barauna.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
